e707cf25a4086ad9a446e6453744bab045bbf215
hyeonginju <guddls64@naver.com>
2024-05-12 19:59:21 +0900

    🎨 second, third code lab

--- code_lab_second/material-components-flutter-codelabs-101-starter/mdc_100_series/lib/login.dart
+++ code_lab_second/material-components-flutter-codelabs-101-starter/mdc_100_series/lib/login.dart
@@ -21,6 +21,9 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
+final _usernameController = TextEditingController();
+final _passwordController = TextEditingController();
+
 class _LoginPageState extends State<LoginPage> {
   // TODO: Add text editing controllers (101)
   @override
@@ -36,12 +39,85 @@ class _LoginPageState extends State<LoginPage> {
                 Image.asset('assets/diamond.png'),
                 const SizedBox(height: 16.0),
                 const Text('SHRINE'),
+                // const SizedBox(height: 120.0),
+                // const TextField(
+                //   decoration: InputDecoration(
+                //     hintText: "Username",
+                //     filled: true,
+                //   ),
+                // ),
+                // const SizedBox(height: 10.0),
+                // const TextField(
+                //   decoration: InputDecoration(
+                //     hintText: "Password",
+                //     filled: true,
+                //   ),
+                // ),
+                // const SizedBox(height: 20.0),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.end,
+                //   children: [
+                //     const Text(
+                //       "CANSEL",
+                //       style: TextStyle(color: Colors.blue),
+                //     ),
+                //     const SizedBox(width: 20),
+                //     Container(
+                //         decoration:
+                //             BoxDecoration(color: Colors.grey[300], boxShadow: [
+                //           BoxShadow(
+                //             offset: Offset(1.5, 1.5),
+                //             blurRadius: 1,
+                //             color: Colors.grey.withOpacity(0.8),
+                //           ),
+                //         ]),
+                //         padding: const EdgeInsets.all(6),
+                //         child: Text("NEXT")),
+                //   ],
+                // )
               ],
             ),
             const SizedBox(height: 120.0),
-            // TODO: Remove filled: true values (103)
-            // TODO: Add TextField widgets (101)
-            // TODO: Add button bar (101)
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            OverflowBar(
+              alignment: MainAxisAlignment.end,
+              // TODO: Add a beveled rectangular border to CANCEL (103)
+              children: <Widget>[
+                // TODO: Add buttons (101)
+
+                TextButton(
+                  child: const Text('CANCEL'),
+                  onPressed: () {
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                ),
+
+                // TODO: Add an elevation to NEXT (103)
+                // TODO: Add a beveled rectangular border to NEXT (103)
+                ElevatedButton(
+                  child: const Text('NEXT'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
           ],
         ),
       ),
